4c109054a08e29a25fc4a144083ff54a5b415f85
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2023-11-25 20:36:59 -0500

    Refactor theme handling in account and pages components

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SettingsService } from 'src/app/services/settings.service';
 
 @Component({
   selector: 'app-account',
@@ -6,35 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.css'],
 })
 export class AccountComponent implements OnInit {
-  public linkTheme = document.querySelector('#theme');
-  public links: NodeListOf<Element> | undefined;
+  constructor(private settingsService: SettingsService) {}
 
   ngOnInit(): void {
-    this.links = document.querySelectorAll('.selector');
-    this.handleCheckCurrentTheme();
+    this.settingsService.handleCheckCurrentTheme();
   }
 
   handleChangeTheme(theme: string) {
-    const url = `./assets/css/colors/${theme}.css`;
-
-    this.linkTheme?.setAttribute('href', url);
-    localStorage.setItem('theme', url);
-
-    this.handleCheckCurrentTheme();
-  }
-
-  handleCheckCurrentTheme() {
-    if (this.links) {
-      this.links.forEach((link) => {
-        link.classList.remove('working');
-        const btnTheme = link.getAttribute('data-theme');
-        const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
-        const currentTheme = this.linkTheme?.getAttribute('href');
-
-        if (btnThemeUrl === currentTheme) {
-          link.classList.add('working');
-        }
-      });
-    }
+    this.settingsService.handleChangeTheme(theme);
   }
 }
