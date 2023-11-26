902fb3c527684447feca1b8e58fb6852c2359fc9
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2023-11-25 20:24:08 -0500

    Fix theme change functionality in account and pages components

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+  public linkTheme = document.querySelector('#theme');
+  public links: NodeListOf<Element> | undefined;
 
+  ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.handleCheckCurrentTheme();
+  }
+
+  handleChangeTheme(theme: string) {
+    const url = `./assets/css/colors/${theme}.css`;
+
+    this.linkTheme?.setAttribute('href', url);
+    localStorage.setItem('theme', url);
+
+    this.handleCheckCurrentTheme();
+  }
+
+  handleCheckCurrentTheme() {
+    if (this.links) {
+      this.links.forEach((link) => {
+        link.classList.remove('working');
+        const btnTheme = link.getAttribute('data-theme');
+        const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+        const currentTheme = this.linkTheme?.getAttribute('href');
+
+        if (btnThemeUrl === currentTheme) {
+          link.classList.add('working');
+        }
+      });
+    }
+  }
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
-  styleUrls: ['./pages.component.css']
+  styleUrls: ['./pages.component.css'],
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit {
+  public linkTheme = document.querySelector('#theme');
 
+  ngOnInit(): void {
+    const url =
+      localStorage.getItem('theme') || './assets/css/colors/purple-dark.css';
+
+    this.linkTheme?.setAttribute('href', url);
+  }
 }
