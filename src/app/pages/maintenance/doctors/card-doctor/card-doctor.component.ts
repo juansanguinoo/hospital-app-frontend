211f067423b1a01434214d2bbba11b395cd052db
Juan David Sanguino <juandavidsanguinoo@gmail.com>
2023-12-07 14:30:00 -0500

    Add form validation and hospital selection in card-doctor component

--- src/app/pages/maintenance/doctors/card-doctor/card-doctor.component.ts
+++ src/app/pages/maintenance/doctors/card-doctor/card-doctor.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Hospital } from 'src/app/models/hospital.model';
+import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
   selector: 'app-card-doctor',
   templateUrl: './card-doctor.component.html',
   styles: [],
 })
-export class CardDoctorComponent {}
+export class CardDoctorComponent implements OnInit {
+  public doctorForm: FormGroup = new FormGroup({});
+  public hospitals: Hospital[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private hospitalService: HospitalService
+  ) {}
+
+  ngOnInit(): void {
+    this.formInit();
+    this.getHospitals();
+  }
+
+  formInit(): void {
+    this.doctorForm = this.fb.group({
+      name: ['', Validators.required],
+      hospital: ['', Validators.required],
+    });
+  }
+
+  handleSubmit(): void {
+    console.log(this.doctorForm.value);
+  }
+
+  getHospitals(): void {
+    this.hospitalService.getHospitals().subscribe((res) => {
+      this.hospitals = res;
+    });
+  }
+}
